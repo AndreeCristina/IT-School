9bcba4c67431bd40077d36e92edae104afb4bdd5
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-10 21:18:02 +0300

    Session 3 - java operators

--- src/main/java/sesion_3_java_operators/ArithmeticOperations.java
+++ src/main/java/sesion_3_java_operators/ArithmeticOperations.java
@@ -24,4 +24,19 @@ public class ArithmeticOperations {
 
 
     }
+
+    public static class LogicalComplementAndNegotiationOperators {
+        public static void main(String[] args) {
+            boolean isFirstTime = true;
+            boolean isRegistered = false;
+
+            System.out.println("Default value: isFirstTime " + isFirstTime);
+            System.out.println("Updated value: isFirstTime " + !isFirstTime);
+
+            System.out.println("Default value: isRegistered " + isRegistered);
+            System.out.println("Updated value: isRegistered " + !isRegistered);
+
+
+        }
+    }
 }
