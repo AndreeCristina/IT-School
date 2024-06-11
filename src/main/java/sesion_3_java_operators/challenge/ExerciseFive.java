c0476920a931d1ab422e9986b5edcd6d42b2d2e0
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-11 15:12:44 +0300

    exercise 2

---
+++ src/main/java/sesion_3_java_operators/challenge/ExerciseFive.java
@@ -0,0 +1,27 @@
+package sesion_3_java_operators.challenge;
+
+public class ExerciseFive {
+
+    public static void main(String[] args) {
+
+        int no1 = 10;
+        no1 += 15;
+        System.out.println(no1);
+
+        int no2 = 6;
+        no2 -= 3;
+        System.out.println(no2);
+
+        int no3 = 15;
+        no3 /= 3;
+        System.out.println(no3);
+
+        int no4 = 21;
+        no4 %= 10;
+        System.out.println(no4);
+
+        int no5 = 5;
+        no5 *= 5;
+        System.out.println(no5);
+    }
+}
