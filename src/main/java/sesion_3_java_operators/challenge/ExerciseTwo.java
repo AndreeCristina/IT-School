4d5680b3a4460ac2a092c28cbfb97f0d0f627405
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-11 13:13:55 +0300

    Session 3 - java operators

---
+++ src/main/java/sesion_3_java_operators/challenge/ExerciseTwo.java
@@ -0,0 +1,24 @@
+package sesion_3_java_operators.challenge;
+
+public class ExerciseTwo {
+
+    public static void main(String[] args) {
+        int number = 99;
+
+        // version 1
+//        System.out.println(++number);
+//        System.out.println(--number);
+//        System.out.println(number++);
+//        System.out.println(number--);
+
+        // version 2
+        ++number;
+        System.out.println(number);
+        --number;
+        System.out.println(number);
+        number++;
+        System.out.println(number);
+        number--;
+        System.out.println(number);
+    }
+}
