a68540b22e581903c3d7d9d512f259b6028a3ce0
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-11 15:22:06 +0300

    exercise 2

--- src/main/java/sesion_3_java_operators/challenge/ExerciseSix.java
+++ src/main/java/sesion_3_java_operators/challenge/ExerciseSix.java
@@ -6,10 +6,12 @@ public class ExerciseSix {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Please, enter the first number: ");
         int firstNumber = sc.nextInt();
         System.out.println("Please, enter the second number: ");
         int secondNumber = sc.nextInt();
+
         if (firstNumber > secondNumber) {
             System.out.println("The first number is greater than the second.");
         } else if (firstNumber == secondNumber) {
