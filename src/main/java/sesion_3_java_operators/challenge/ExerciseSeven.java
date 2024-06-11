f197b9000f9ff3ccd1de04c37811c61a63d246d6
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-11 20:51:15 +0300

    exercise 7

--- src/main/java/sesion_3_java_operators/challenge/ExerciseSeven.java
+++ src/main/java/sesion_3_java_operators/challenge/ExerciseSeven.java
@@ -4,8 +4,10 @@ public class ExerciseSeven {
     public static void main(String[] args) {
         // 7. Write a program where you perform operations between an integer and a double.
         // Display the result.
+
         int no1 = 3;
         double no2 = 8;
+
         System.out.println("Sum: " + (no1 + no2));
         System.out.println("Difference: " + (no1 - no2));
         System.out.println("Product: " + (no1 * no2));
