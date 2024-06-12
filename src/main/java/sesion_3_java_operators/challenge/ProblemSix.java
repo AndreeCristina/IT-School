5a17a3857213afb2122e833ea64433b9598bed77
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-12 20:10:29 +0300

    Problem6

--- src/main/java/sesion_3_java_operators/challenge/ProblemSix.java
+++ src/main/java/sesion_3_java_operators/challenge/ProblemSix.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ProblemSix {
 
     public static void main(String[] args) {
-        /*6: Age Verification. Write a Java program to verify if a person is eligible to vote.
+        /*6:Age Verification. Write a Java program to verify if a person is eligible to vote.
         Given a person's age, if the age is 18 or above, print "Eligible to vote", otherwise print "Not eligible to vote".*/
 
         Scanner sc = new Scanner(System.in);
