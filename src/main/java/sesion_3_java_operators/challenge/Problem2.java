59de3c8e9f1aaaf5f1fce711f65fe78dd6744d39
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-12 19:23:25 +0300

    Problem1

---
+++ src/main/java/sesion_3_java_operators/challenge/Problem2.java
@@ -0,0 +1,17 @@
+package sesion_3_java_operators.challenge;
+
+import java.util.Scanner;
+
+public class Problem2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, enter the temperature in Celsius degrees:");
+        double tempCelsius = sc.nextDouble();
+
+        double tempFahrenheit = (tempCelsius * 9 / 5) + 32;
+
+        System.out.println("Temperature in Fahrenheit degrees is: " + tempFahrenheit);
+    }
+}
