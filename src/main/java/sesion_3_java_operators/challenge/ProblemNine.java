241c38a113427eca1250c57ef5ada956d42a6f4d
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-13 10:45:57 +0300

    Problem9

---
+++ src/main/java/sesion_3_java_operators/challenge/ProblemNine.java
@@ -0,0 +1,22 @@
+package sesion_3_java_operators.challenge;
+
+import java.util.Scanner;
+
+public class ProblemNine {
+
+    public static void main(String[] args) {
+        /* 9.Currency Conversion. Write a Java program to convert an amount from dollars to euros.
+        Given the amount in dollars and the conversion rate conversionRate, calculate and print the amount in euros. */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, enter the amount in dollars:");
+        double dollars = sc.nextDouble();
+        System.out.println("Please, enter the conversion rate:");
+        double conversionRate = sc.nextDouble();
+
+        double amountInEuros = dollars * conversionRate;
+
+        System.out.println("For " + dollars + " dollars you will get " + amountInEuros + " euros.");
+    }
+}
