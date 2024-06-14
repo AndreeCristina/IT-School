7837775d35a10616be5cd05d94a364672ca9ab2b
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-14 20:06:57 +0300

    exercise15

--- src/main/java/sesion_4_java_operators_loops/challenge/ExerciseFifteen.java
+++ src/main/java/sesion_4_java_operators_loops/challenge/ExerciseFifteen.java
@@ -5,7 +5,7 @@ public class ExerciseFifteen {
     public static void main(String[] args) {
         /* 15. Write a Java program where you declare an integer variable total with a value of 100.
         Use the compound assignment operator to decrease the total by 20 and then print the value.
-        Next, use another compound assignment operator to multiply the total by 2 and then print the value.*/
+        Next, use another compound assignment operator to multiply the total by 2 and then print the value. */
         int total = 100;
 
         total -= 20;
