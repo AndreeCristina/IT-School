d6fdec814547289a9847e1fcc0fa48eeb5c00a95
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-18 15:05:48 +0300

    sesion 5 challenge ex 8

--- src/main/java/sesion_5_advanced_flow_control/challenge/Exercise8.java
+++ src/main/java/sesion_5_advanced_flow_control/challenge/Exercise8.java
@@ -5,11 +5,11 @@ public class Exercise8 {
     public static void main(String[] args) {
         /* 8. For Loop. Write a program that prints the first 10 numbers in the Fibonacci series. Use a for loop to
         accomplish this.*/
-        int firstNo = 0, secondNo = 1;
+        int firstNo = 1, secondNo = 1;
 
-        System.out.print(1 + " ");
+        System.out.print(firstNo + " " + secondNo + " ");
 
-        for (int i = 2; i <= 10; ++i) {
+        for (int i = 3; i <= 10; ++i) {
             int nextNo = firstNo + secondNo;
             firstNo = secondNo;
             secondNo = nextNo;
