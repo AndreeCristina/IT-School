407bbc41c2e5576bd44520901a39f5c883ff1a35
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-19 17:59:58 +0300

    sesion 6 challenge ex 2

---
+++ src/main/java/sesion_6_java_APIs/challenge/Exercise2.java
@@ -0,0 +1,24 @@
+package sesion_6_java_APIs.challenge;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        /* Palindrome Checker Description: Write a program that checks whether a given string is a palindrome
+        using the StringBuilder class. A palindrome is a word, phrase, number, or other sequences of characters
+        that reads the same forward and backward.*/
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, enter a word:");
+        String myWord = sc.nextLine();
+
+        StringBuilder myReversedWord = new StringBuilder(myWord).reverse();
+
+        if (myWord.equalsIgnoreCase(myReversedWord.toString())) {
+            System.out.println("The string " + myWord + " is palindrome.");
+        } else {
+            System.out.println("The string " + myWord + " is not palindrome.");
+        }
+    }
+}
