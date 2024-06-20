ea8fecfeaa77ce4b1beeb2701e77ffb2f9ea53cf
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-20 21:11:31 +0300

    sesion 6 practice

---
+++ src/main/java/sesion_6_java_core_apis/practice/library_system/Product.java
@@ -0,0 +1,10 @@
+package sesion_6_java_core_apis.practice.library_system;
+
+public class Product {
+
+    private String id;
+    private String type;
+    private String name;
+    private String description;
+    private double price;
+}
---
+++ src/main/java/sesion_6_java_core_apis/practice/library_system/PurchaseHistory.java
@@ -0,0 +1,6 @@
+package sesion_6_java_core_apis.practice.library_system;
+
+public class PurchaseHistory {
+
+
+}
