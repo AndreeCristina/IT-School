27720734058f53c225879c009d4a9ba6ae56b834
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-26 12:17:58 +0300

    sesion 7 challenge 9

---
+++ src/main/java/sesion_7_utility_classes/challenge/Exercise9.java
@@ -0,0 +1,28 @@
+package sesion_7_utility_classes.challenge;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+public class Exercise9 {
+    /* 9. Interval Between Dates Description: Design a method titled daysBetween that reads two dates from the console
+    and computes the total number of days between them. Input: Two dates given by the user in the format YYYY-MM-DD.
+    Expected Output: An integer showcasing the difference in days between the two dates.*/
+    public static void main(String[] args) {
+        System.out.println(daysBetween());
+    }
+
+    public static long daysBetween() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, enter the first date:");
+        String firstDate = sc.nextLine();
+        System.out.println("Please, enter the second date:");
+        String secondDate = sc.nextLine();
+
+        LocalDate parsedFirstDate = LocalDate.parse(firstDate);
+        LocalDate parsedSecondDate = LocalDate.parse(secondDate);
+
+        return ChronoUnit.DAYS.between(parsedFirstDate, parsedSecondDate);
+    }
+}
