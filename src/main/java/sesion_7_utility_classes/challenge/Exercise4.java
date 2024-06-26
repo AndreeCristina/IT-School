c2c82cc185a0ce915bc9620414b5fc74cc4377e1
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-26 10:51:41 +0300

    sesion 7 challenge 4

--- src/main/java/sesion_7_utility_classes/challenge/Exercise4.java
+++ src/main/java/sesion_7_utility_classes/challenge/Exercise4.java
@@ -1,22 +1,32 @@
 package sesion_7_utility_classes.challenge;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Exercise4 {
     /* 4. Comparing User-Entered Dates Description: Write a method named areDatesEqual that reads two dates from the console
      and returns true if they are equal, and false otherwise.Input: Two dates entered by the user in the format YYYY-MM-DD.
      Expected Output: true if both dates are equal. false if they are different.*/
-    public static void areDatesEqual(String firstDate, String secondDate) {
-
+    public static void main(String[] args) {
+        areDatesEqual();
     }
 
-    public static void main(String[] args) {
+    public static void areDatesEqual() {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Please, enter the first date (YYYY-MM-DD):");
-        String firstDate = sc.next();
+        String firstDate = sc.nextLine();
         System.out.println("Please, enter the second date (YYYY-MM-DD)");
-        String secondDate = sc.next();
+        String secondDate = sc.nextLine();
 
+        LocalDate parsedFirstDate = LocalDate.parse(firstDate);
+        LocalDate parsedSecondDate = LocalDate.parse(secondDate);
+
+        boolean compareDates = parsedFirstDate.isEqual(parsedSecondDate);
+        System.out.println(compareDates);
+//        if (parsedFirstDate.isEqual(parsedSecondDate)) {
+//            System.out.println(true);
+//        } else {
+//            System.out.println(false);
+//        }
     }
 }
