cd1e3cb4e974fafd7227a8d5fe07990a6bde6877
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-26 10:36:32 +0300

    sesion 7 challenge 3

---
+++ src/main/java/sesion_7_utility_classes/challenge/Exercise4.java
@@ -0,0 +1,22 @@
+package sesion_7_utility_classes.challenge;
+
+import java.util.Scanner;
+
+public class Exercise4 {
+    /* 4. Comparing User-Entered Dates Description: Write a method named areDatesEqual that reads two dates from the console
+     and returns true if they are equal, and false otherwise.Input: Two dates entered by the user in the format YYYY-MM-DD.
+     Expected Output: true if both dates are equal. false if they are different.*/
+    public static void areDatesEqual(String firstDate, String secondDate) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, enter the first date (YYYY-MM-DD):");
+        String firstDate = sc.next();
+        System.out.println("Please, enter the second date (YYYY-MM-DD)");
+        String secondDate = sc.next();
+
+    }
+}
