f8f02f4b6ca3f1c017d712abc01cbf9a59879694
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-26 11:07:40 +0300

    sesion 7 challenge 5

---
+++ src/main/java/sesion_7_utility_classes/challenge/Exercise5.java
@@ -0,0 +1,19 @@
+package sesion_7_utility_classes.challenge;
+
+import java.time.LocalDate;
+
+public class Exercise5 {
+    /* 5. Is Today a Specific Date? Description: Write a method named isTodaySpecificDate that checks if today's date
+    is 10th December 2019. Expected Output: true if today's date is 2019-12-10. false otherwise.*/
+    public static void main(String[] args) {
+        LocalDate specificDate = LocalDate.of(2019, 12, 10);
+
+        String compareDates = isTodaySpecificDate(specificDate) ? "true" : "false";
+        System.out.println(compareDates);
+    }
+
+    public static boolean isTodaySpecificDate(LocalDate specificDate) {
+        LocalDate today = LocalDate.now();
+        return today.equals(specificDate);
+    }
+}
