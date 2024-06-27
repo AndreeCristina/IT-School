6dfb6c4e9981a368dcdf083ccf2e08047e78174f
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-27 12:15:04 +0300

    sesion 7 challenge 10

--- src/main/java/sesion_7_utility_classes/challenge/Exercise10ToRebuild.java
+++ src/main/java/sesion_7_utility_classes/challenge/Exercise10ToRebuild.java
@@ -2,9 +2,10 @@ package sesion_7_utility_classes.challenge;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
-public class Exercise10deRefacut {
+public class Exercise10ToRebuild {
     /* 10. Date Authenticator Description: Engineer a method named isValidDate that captures a date string from the console
     and assesses if it conforms to a valid date format (YYYY-MM-DD). The method should also consider leap years when
     validating February dates. Input: A string of a date from the console. Expected Output: true if the input adheres
@@ -15,28 +16,17 @@ public class Exercise10deRefacut {
 
         System.out.println("Please, enter a date (yyyy-MM-dd): ");
         String dateString = sc.nextLine();
+
+        boolean valid = isValidDate(dateString);
+        System.out.println("Is the date valid? " + valid);
     }
 
     public static boolean isValidDate(String dateString) {
-
-        LocalDate parsedDate = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
-        if (parsedDate.getMonthValue() < 1 || parsedDate.getMonthValue() > 12) {
+        try {
+            LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            return true;
+        } catch (DateTimeParseException e) {
             return false;
         }
-
-        int day = parsedDate.getDayOfMonth();
-        if (day < 1 || day> 31) {
-            return false;
-        } else if (parsedDate.getMonthValue() == 4 || parsedDate.getMonthValue() == 6 || parsedDate.getMonthValue() == 9 || parsedDate.getMonthValue() == 11) {
-            if (day == 30) {
-                return false;
-            } else if (parsedDate.getMonthValue() == 2) {
-                if (parsedDate.getDayOfMonth() == 29 && parsedDate.isLeapYear()) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
