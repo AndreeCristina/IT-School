6812a66b9e18210a1705f75ce5b3305c1aa2e6a3
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-06-27 21:04:33 +0300

    sesion 8

---
+++ src/main/java/sesion_8_recap/practice/FirstLetter.java
@@ -0,0 +1,20 @@
+package sesion_8_recap.practice;
+
+public class FirstLetter {
+    /* Capitalize First Letter Description: Write a method that takes a string and capitalizes the first letter of each word.
+    Use loops and string manipulation methods. Test the method with different strings and print the results.*/
+
+    public static void main(String[] args) {
+        String input = "apple,banana,orange,blueberry";
+        String[] inputArray = input.split(",");
+
+        StringBuilder sb = new StringBuilder();
+        for (String word : inputArray) {
+            sb.append(Character.toUpperCase(word.charAt(0)))
+                    .append(word.substring(1))
+                    .append(",");
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ src/main/java/sesion_8_recap/practice/MethodReturnType.java
@@ -0,0 +1,24 @@
+package sesion_8_recap.practice;
+
+public class MethodReturnType {
+
+    public static void main(String[] args) {
+        int intValue = getInt();
+        System.out.println(intValue);
+
+        printInt();
+
+    }
+
+    public static int getInt() {
+        return 10;
+    }
+
+    public static void printInt() {
+        System.out.println(352);
+    }
+
+    public void testMethod() {
+
+    }
+}
