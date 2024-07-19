3a1ab0cd1477ffe2c5d2d86d0cd632637dae3699
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-07-19 15:17:50 +0300

    challenge 4 session 13

---
+++ src/main/java/session_13_abstraction/challenge/challenge4/AbstractClass.java
@@ -0,0 +1,14 @@
+package session_13_abstraction.challenge.challenge4;
+
+public abstract class AbstractClass {
+
+    public AbstractClass() {
+        System.out.println("This is constructor of abstract class");
+    }
+
+    public abstract void a_method();
+
+    public void normal_method() {
+        System.out.println("This is a normal method of abstract class");
+    }
+}
