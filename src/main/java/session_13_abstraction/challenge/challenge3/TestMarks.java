919bfc52001eb5b27f9377b5df0060032a198b94
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-07-19 14:46:28 +0300

    challenge session 13

---
+++ src/main/java/session_13_abstraction/challenge/challenge3/TestMarks.java
@@ -0,0 +1,12 @@
+package session_13_abstraction.challenge.challenge3;
+
+public class TestMarks {
+
+    public static void main(String[] args) {
+        StudentA studentA = new StudentA(71, 54, 89);
+        StudentB studentB = new StudentB(100, 86, 97, 76);
+
+        System.out.println("StudentA percentage is: " + studentA.getPercentage() + " %");
+        System.out.println("StudentB percentage is: " + studentB.getPercentage() + " %");
+    }
+}
