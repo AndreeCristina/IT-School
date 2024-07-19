0a0862a4f1d2fc2b319ca7d467c242e0ecc37d4d
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-07-19 12:41:53 +0300

    KATA 4

--- src/main/java/kata/session_4/ReverseNumberWhile.java
+++ src/main/java/kata/session_4/ReverseNumberWhile.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class ReverseNumberWhile {
 
+    static final int TEN = 10;
+    static final int MAX_VALUE = 1000000000;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a positive number");
         int number = sc.nextInt();
 
-        if (number > 0 && number < 1000000000 && (number % 10 == 0)) {
-            System.out.println("Please enter a valid number: number between 0 and 1000000000 and the last digit is by 0");
-        }
-
         int reverseNo = 0;
-
-        while (number != 0) {
-            int lastDigit = number % 10;
-            reverseNo = reverseNo * 10 + lastDigit;
-            number /= 10;
+        if (number > 0 && number < MAX_VALUE && (number % TEN == 0)) {
+            System.out.println("Please enter a valid number: number between 0 and 1000000000 and the last digit of the number is different from 0");
+        } else {
+            while (number != 0) {
+                reverseNo = reverseNo * TEN + number % TEN;
+                number /= TEN;
+            }
+            System.out.println("The reversed number is: " + reverseNo);
         }
-
-        System.out.println("The reversed number is: " + reverseNo);
     }
 }
