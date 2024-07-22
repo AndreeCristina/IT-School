64f1639776455c5b9a77288c37835a2a001b88e1
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-07-22 21:22:04 +0300

    session15

--- src/main/java/session_13_abstraction/challenge/challenge5/Animal.java
+++ src/main/java/session_13_abstraction/challenge/challenge5/Animal.java
@@ -2,7 +2,5 @@ package session_13_abstraction.challenge.challenge5;
 
 public abstract class Animal {
 
-    public abstract void cats();
-
-    public abstract void dogs();
+    public abstract void makeSound();
 }
--- src/main/java/session_13_abstraction/challenge/challenge5/Cat.java
+++ src/main/java/session_13_abstraction/challenge/challenge5/Cat.java
@@ -3,12 +3,7 @@ package session_13_abstraction.challenge.challenge5;
 public class Cat extends Animal {
 
     @Override
-    public void cats() {
+    public void makeSound() {
         System.out.println("Cats meow");
-    }
-
-    @Override
-    public void dogs() {
-
     }
 }
