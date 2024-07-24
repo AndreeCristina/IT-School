d95c1911098d9f6161e3ed6e253f12e963215497
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-07-24 18:59:22 +0300

    exercise 11 challenge loop and flow control

---
+++ src/main/java/session_15_oop_recap/challenge/loops_and_flow_control/Exercise11.java
@@ -0,0 +1,21 @@
+package session_15_oop_recap.challenge.loops_and_flow_control;
+
+public class Exercise11 {
+    /* Write a Java program to find the sum of the series: 1 - 2 + 3 - 4 + 5 - 6 + ... up to n
+    terms using a loop. Inside your loop, utilize pre and post increment operators to manage your loop
+    counter and to manipulate the values in the series. */
+
+    public static void main(String[] args) {
+        int n = 10;
+
+        int sum = 0;
+        int sign = 1;
+
+        for (int index = 1; index <= n; ++index) {
+            sum += sign * index;
+            sign = -sign;
+        }
+
+        System.out.println("Sum of the series: " + sum);
+    }
+}
