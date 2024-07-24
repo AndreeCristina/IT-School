d73b430a80f89682a182317c75861cb48fd43e8a
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-07-24 09:22:15 +0300

    challenge 12 javaBasics

---
+++ src/main/java/session_15_oop_recap/challenge/javaBasics/Exercise12.java
@@ -0,0 +1,25 @@
+package session_15_oop_recap.challenge.javaBasics;
+
+public class Exercise12 {
+    /* Default Values: Declare variables of different data types without initializing them.
+    Print out their default values to verify that they were initialized correctly.*/
+    static boolean booleanVariable;
+    static char charVariable;
+    static byte byteVariable;
+    static short shortVariable;
+    static int intVariable;
+    static long longVariable;
+    static float floatVariable;
+    static double doubleVariable;
+
+    public static void main(String[] args) {
+        System.out.println("Default value for boolean variable: " + booleanVariable);
+        System.out.println("Default value for char variable: " + charVariable);
+        System.out.println("Default value for byte variable: " + byteVariable);
+        System.out.println("Default value for short variable: " + shortVariable);
+        System.out.println("Default value for int variable: " + intVariable);
+        System.out.println("Default value for long variable: " + longVariable);
+        System.out.println("Default value for float variable: " + floatVariable);
+        System.out.println("Default value for double variable: " + doubleVariable);
+    }
+}
