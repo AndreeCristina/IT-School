57c6411935e2f0b39085a1fd4e9bf6be5495d083
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-07-27 11:46:25 +0300

    session 16 challenge linked list exercises 14

--- src/main/java/session_16_collections_framework/challenge/linked_list/Exercise14.java
+++ src/main/java/session_16_collections_framework/challenge/linked_list/Exercise14.java
@@ -1,9 +1,31 @@
 package session_16_collections_framework.challenge.linked_list;
 
+import java.util.LinkedList;
+
 public class Exercise14 {
     // Write a Java program to remove all the elements from a linked list.
 
     public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("New York");
+        placesToVisit.add("London");
+        placesToVisit.add("Madrid");
+        placesToVisit.add("Milan");
 
+        System.out.println("Original linked list: ");
+        displayPlaceToVisit(placesToVisit);
+
+        placesToVisit.clear();
+
+        System.out.println("Linked list after remove all the elements: ");
+        displayPlaceToVisit(placesToVisit);
+    }
+
+    public static void displayPlaceToVisit(LinkedList<String> placeToVisit) {
+        for (String place : placeToVisit) {
+            System.out.print(place + " ");
+        }
+        System.out.println();
     }
 }
