53e5a1831265f93d7f09d85ca1d0f421b48f8cb5
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-07-29 18:39:19 +0300

    session 16 challenge TreeSet

---
+++ src/main/java/session_16_collections_framework/challenge/hash_map/Exercise1.java
@@ -0,0 +1,9 @@
+package session_16_collections_framework.challenge.hash_map;
+
+public class Exercise1 {
+    // Write a Java program to associate the specified value with the specified key in a HashMap.
+    
+    public static void main(String[] args) {
+
+    }
+}
