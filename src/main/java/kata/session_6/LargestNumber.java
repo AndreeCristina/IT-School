e75b01e9ea5a242cc2cf26ae2fa741cd67134516
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-08-07 19:55:14 +0300

    challenge session 1

---
+++ src/main/java/kata/session_6/LargestNumber.java
@@ -0,0 +1,31 @@
+package kata.session_6;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, enter the first number: ");
+        int number1 = sc.nextInt();
+
+        System.out.println("Please, enter the second number: ");
+        int number2 = sc.nextInt();
+
+        System.out.println("Please, enter the third number: ");
+        int number3 = sc.nextInt();
+
+        int theLargestNo;
+
+        if (number1 >= number2 && number1 >= number3) {
+            theLargestNo = number1;
+        } else if (number2 >= 1 && number2 >= number3) {
+            theLargestNo = number2;
+        } else {
+            theLargestNo = number3;
+        }
+
+        System.out.println("The largest number is: " + theLargestNo);
+    }
+}
---
+++ src/main/java/kata/session_6/README.md
@@ -0,0 +1,3 @@
+Find the Largest Number
+Description: Write a program that takes three integer inputs from the user and finds the largest among them. 
+The program should print the largest number.
