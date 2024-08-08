d9f0c5f365e917bcb8b32d19f8050d401aea40a9
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-08-08 10:44:59 +0300

    challenge session16

---
+++ src/main/java/session_19/challenge/exercise_5/Exercise3.java
@@ -0,0 +1,33 @@
+package session_19.challenge.exercise_5;
+
+import java.util.stream.IntStream;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 100)
+                .filter(Exercise3::isPrime)
+                .forEach(System.out::println);
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+
+        int i = 5;
+        while (i * i <= number) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+            i += 6;
+        }
+        return true;
+    }
+}
