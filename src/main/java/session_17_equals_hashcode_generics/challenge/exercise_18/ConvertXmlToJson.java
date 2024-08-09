321e2f15b40b03accf7daff817325d81346f9f66
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-08-09 12:58:41 +0300

    challenges session 17 XML FILE PATH

--- src/main/java/session_17_equals_hashcode_generics/challenge/exercise_18/ConvertXmlToJson.java
+++ src/main/java/session_17_equals_hashcode_generics/challenge/exercise_18/ConvertXmlToJson.java
@@ -7,9 +7,10 @@ import java.io.File;
 import java.nio.file.Files;
 
 public class ConvertXmlToJson {
+    private static final String XML_FILE_PATH = "C:\\Personal\\IT School\\src\\main\\java\\session_17_equals_hashcode_generics\\challenge\\exercise_18\\bookstore.xml";
 
     public static void main(String[] args) throws Exception {
-        File xmlFile = new File("C:\\Personal\\IT School\\src\\main\\java\\session_17_equals_hashcode_generics\\challenge\\exercise_18\\bookstore.xml");
+        File xmlFile = new File(XML_FILE_PATH);
 
         byte[] b = Files.readAllBytes(xmlFile.toPath());
 
